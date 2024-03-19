64096c96059fa5ebda5161cc1ef931a893808d63
Lucifergod507 <131298140+Lucifergod507@users.noreply.github.com>
2024-03-19 13:21:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import os
 if os.path.exists('.env'):
     load_dotenv(override=True)
 
-API_ID = os.getenv('API_ID')
-API_HASH = os.getenv('API_HASH')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+API_ID = os.getenv('9407706')
+API_HASH = os.getenv('0b75c89e269ad46f30bae2f13fd8c700')
+BOT_TOKEN = os.getenv('7117076232:AAEgKgeGdCV3WagWTiZyWeT80aODTVEKZoY')
 ALLOWED_CHATS = [int(chat_id) for chat_id in os.getenv('ALLOWED_CHATS').split(' ')]
 
 ASSETS_DIR = os.path.join(os.getcwd(), 'assets')
